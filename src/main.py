c7176615bcd7a6e66986f9b650cc0b73155ef15b
Sadegh Jafari <sadeghjafari5079@gmail.com>
2026-01-03 22:50:33 +0100

    add dari

--- src/main.py
+++ src/main.py
@@ -1,25 +1,13 @@
 import pandas as pd
-from tqdm import tqdm
 
-from utils import translate_text
 
-dezfuli_path = '/home/sadegh/Translation/data/dezfuli.xlsx'
-lori_path = '/home/sadegh/Translation/data/lori.xlsx'
-mazandaran_tonekaboni_path = '/home/sadegh/Translation/data/Mazandaran_Tonekaboni.xlsx'
-semnani_path = '/home/sadegh/Translation/data/Semnani.xlsx'
-southern_kurdish_kalhori_path = '/home/sadegh/Translation/data/Southern_Kurdish_Kalhori.xlsx'
-zoroastrian_yazdi_path = '/home/sadegh/Translation/data/Zoroastrian_Yazdi.xlsx'
+new_path = '/home/sadegh/Translation/datasets/translation_data_simplified.csv'
 
-data_path = '/home/sadegh/Translation/datasets/data.csv'
+data_path = '/home/sadegh/Translation/datasets/translation_data.csv'
 
-dezfuli_df = pd.read_excel(dezfuli_path)
-lori_df = pd.read_excel(lori_path)
-mazandaran_tonekaboni_df = pd.read_excel(mazandaran_tonekaboni_path)
-semnani_df = pd.read_excel(semnani_path)
-southern_kurdish_kalhori_df = pd.read_excel(southern_kurdish_kalhori_path)
-zoroastrian_yazdi_df = pd.read_excel(zoroastrian_yazdi_path)
+new_data = pd.read_csv(new_path)
 
 data = pd.read_csv(data_path)
-data['zoroastrian_yazdi_translation'] = zoroastrian_yazdi_df['translation']
-data['zoroastrian_yazdi_transliteration'] = zoroastrian_yazdi_df['transliteration']
+data['dari_translation'] = new_data['dari_translation']
+# data['zoroastrian_yazdi_transliteration'] = new_data['transliteration']
 data.to_csv(data_path, index=False)
