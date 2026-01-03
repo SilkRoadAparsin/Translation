48b55c82c54a2367ab064312c8dcc33976d6c824
Sadegh Jafari <sadeghjafari5079@gmail.com>
2026-01-03 23:28:19 +0100

    add khorasani

--- src/main.py
+++ src/main.py
@@ -1,14 +1,14 @@
 import pandas as pd
 
 
-new_path = '/home/sadegh/Translation/data/shirazi_annotations/100_samples_shirazi_sentiment_ann1.xlsx'
+new_path = '/home/sadegh/Translation/data/Khorasani_final_dataset_filtered (2).csv'
 
 data_path = '/home/sadegh/Translation/datasets/translation_data.csv'
 
-# new_data = pd.read_csv(new_path)
-new_data = pd.read_excel(new_path)
+new_data = pd.read_csv(new_path)
+# new_data = pd.read_excel(new_path)
 
 data = pd.read_csv(data_path)
-data['shirazi_translation'] = new_data['shirazi_translation']
-data['shirazi_transliteration'] = new_data['shirazi_transliteration']
+data['khorasani_translation'] = new_data['transliteration']
+data['khorasani_transliteration'] = new_data['sent1']
 data.to_csv(data_path, index=False)
