55c97a5422411598a8040d2c2c5bf0575f2825ba
Sadegh Jafari <sadeghjafari5079@gmail.com>
2026-01-03 23:02:53 +0100

    add isfahani

--- src/main.py
+++ src/main.py
@@ -1,13 +1,14 @@
 import pandas as pd
 
 
-new_path = '/home/sadegh/Translation/datasets/translation_data_simplified.csv'
+new_path = '/home/sadegh/Translation/datasets/esfahani_with_sentiments.xlsx'
 
 data_path = '/home/sadegh/Translation/datasets/translation_data.csv'
 
-new_data = pd.read_csv(new_path)
+# new_data = pd.read_csv(new_path)
+new_data = pd.read_excel(new_path)
 
 data = pd.read_csv(data_path)
-data['dari_translation'] = new_data['dari_translation']
-# data['zoroastrian_yazdi_transliteration'] = new_data['transliteration']
+data['isfahani_translation'] = new_data['isfahani']
+data['isfahani_transliteration'] = new_data['transliteration']
 data.to_csv(data_path, index=False)
