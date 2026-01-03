d54b8ce94184ae4b0e28f94f7d23a37d34df2fcd
Sadegh Jafari <sadeghjafari5079@gmail.com>
2026-01-03 23:20:50 +0100

    add shirazi

--- src/main.py
+++ src/main.py
@@ -1,7 +1,7 @@
 import pandas as pd
 
 
-new_path = '/home/sadegh/Translation/datasets/esfahani_with_sentiments.xlsx'
+new_path = '/home/sadegh/Translation/data/shirazi_annotations/100_samples_shirazi_sentiment_ann1.xlsx'
 
 data_path = '/home/sadegh/Translation/datasets/translation_data.csv'
 
@@ -9,6 +9,6 @@ data_path = '/home/sadegh/Translation/datasets/translation_data.csv'
 new_data = pd.read_excel(new_path)
 
 data = pd.read_csv(data_path)
-data['isfahani_translation'] = new_data['isfahani']
-data['isfahani_transliteration'] = new_data['transliteration']
+data['shirazi_translation'] = new_data['shirazi_translation']
+data['shirazi_transliteration'] = new_data['shirazi_transliteration']
 data.to_csv(data_path, index=False)
