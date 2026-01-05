60d27779a9f50852953da6521a21d965958ec1a0
Sadegh Jafari <sadeghjafari5079@gmail.com>
2026-01-05 08:43:41 +0100

    add yazdi

--- src/main.py
+++ src/main.py
@@ -1,14 +1,14 @@
 import pandas as pd
 
 
-new_path = '/home/sadegh/Translation/data/Hazaragi_translation.csv'
+new_path = '/home/sadegh/Translation/data/Yazdi.xlsx'
 
 data_path = '/home/sadegh/Translation/datasets/translation_data.csv'
 
-new_data = pd.read_csv(new_path)
-# new_data = pd.read_excel(new_path)
+# new_data = pd.read_csv(new_path)
+new_data = pd.read_excel(new_path)
 
 data = pd.read_csv(data_path)
-data['hazaragi_translation'] = new_data['Hazaragi_translation']
-data['hazaragi_transliteration'] = new_data['Hazaragi_transliteration']
+data['yazdi_translation'] = new_data['translation']
+data['yazdi_transliteration'] = new_data['transliteration']
 data.to_csv(data_path, index=False)
