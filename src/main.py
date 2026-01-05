8dd1d99bc5629bb0527fd7572c33df189889adab
Sadegh Jafari <sadeghjafari5079@gmail.com>
2026-01-05 08:55:11 +0100

    add pashto

--- src/main.py
+++ src/main.py
@@ -1,7 +1,7 @@
 import pandas as pd
 
 
-new_path = '/home/sadegh/Translation/data/Yazdi.xlsx'
+new_path = '/home/sadegh/Translation/data/pashto.xlsx'
 
 data_path = '/home/sadegh/Translation/datasets/translation_data.csv'
 
@@ -9,6 +9,6 @@ data_path = '/home/sadegh/Translation/datasets/translation_data.csv'
 new_data = pd.read_excel(new_path)
 
 data = pd.read_csv(data_path)
-data['yazdi_translation'] = new_data['translation']
-data['yazdi_transliteration'] = new_data['transliteration']
+data['pashto_translation'] = new_data['Pashto Translation']
+# data['pashto_transliteration'] = new_data['transliteration']
 data.to_csv(data_path, index=False)
